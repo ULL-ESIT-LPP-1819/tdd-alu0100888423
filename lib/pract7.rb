c490334e60426908dd38a96a4b1581b82b4eab15
Eduardo Flores <eduflores@MacBook-Pro-de-Eduardo.local>
2018-11-10 19:33:02 +0000

    Estructura bundle

---
+++ lib/pract7.rb
@@ -0,0 +1,5 @@
+require "pract7/version"
+
+module Pract7
+  # Your code goes here...
+end
---
+++ spec/pract7_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe Pract7 do
+  it "has a version number" do
+    expect(Pract7::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
